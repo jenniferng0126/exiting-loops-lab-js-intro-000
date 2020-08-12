135305cc274e6daa188fdc40d7487a082852b12a
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-12 21:05:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function breakOut(array, changeValue, stopValue) {
   for (let i=0; i<array.length; i++) {
     if (array[i] === stopValue) {
       break;
-    } else {
-      array[i] = changeValue;
     }
+    array[i] = changeValue;
   }
+  return array;
 }
